b43b1e1ee90cd11bd9998982857c4fe11be9eab3
Sal Tamay <sal.tamay@outlook.com>
2020-03-06 20:28:00 -0500

    feat: Get saved books from db using Apollo Client

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
 import BookList from './components/BookList';
+
+const client = new ApolloClient({
+  uri: '/api/v1/graphql'
+});
 
 function App() {
   return (
-    <div id='main'>
-      <h1 className='center-align'>Hello from Google Books!</h1>
-      <BookList />
-    </div>
+    <ApolloProvider client={client}>
+      <div id='main'>
+        <h1 className='center-align'>Hello from Google Books!</h1>
+        <BookList />
+      </div>
+    </ApolloProvider>
   );
 }
 
--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,17 +1,34 @@
 import React, { useState, useEffect } from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
 import Book from './Book';
 import { booksData } from '../temp/books-data';
 
+const SAVED_BOOKS = gql`
+  {
+    books {
+      id
+      title
+      description
+    }
+  }
+`;
+
 function BookList() {
-  const [books, setBooks] = useState([]);
-  useEffect(() => {
-    setBooks(booksData);
-  }, []);
+  // const [books, setBooks] = useState([]);
+  // useEffect(() => {
+  //   setBooks(booksData);
+  // }, []);
+
+  const { loading, error, data } = useQuery(SAVED_BOOKS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
 
   return (
     <div className='container'>
       <div className='row'>
-        {books.map(book => (
+        {data.books.map(book => (
           <Book key={book.title} book={book} />
         ))}
       </div>

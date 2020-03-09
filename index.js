ac7ba1c87311a019d6ab7da2c3cf82a7ffc92c5f
Sal Tamay <sal.tamay@outlook.com>
2020-03-09 14:55:18 -0400

    feat: Add catch all route for server to resend build files

--- index.js
+++ index.js
@@ -13,6 +13,12 @@ app.use('/api/v1/graphql', graphqlHTTP({ schema, graphiql: true }));
 
 require('./database')();
 
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (_req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () =>
